914b3b4a8afa11821c3b26a3bef331da7d05424b
Yuliia Komlyk <juliajuli99933@gmail.com>
2024-02-12 03:38:50 +0200

    Final results were addded

--- hw12/script.js
+++ hw12/script.js
@@ -1,9 +1,12 @@
+// Open/close burger menu
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("nav__burger-btn").addEventListener("click", function() {
       document.querySelector(".nav").classList.toggle("open")
     })
 });
 
+
+// Closing the menu by clicking in the area around
 
 document.getElementById("nav-links__list").addEventListener('click', event => {
   event._isClickWithInMenu = true;
@@ -15,3 +18,35 @@ document.body.addEventListener('click', event => {
   if (event._isClickWithInMenu) return;
   document.querySelector(".nav").classList.remove("open")
 });
+
+
+// // Timetraker
+
+function addZero(currentNumber) {
+  if(currentNumber < 10) {
+    return "0" + currentNumber;
+  } else {
+    return currentNumber;
+  }
+}
+
+function getCurrentTime() {
+  const date = new Date();
+
+  const fullTime =
+    addZero(date.getHours()) +
+    " : " +
+    addZero(date.getMinutes()) +
+    " : " +
+    addZero(date.getSeconds());
+
+  console.log(fullTime);
+  return fullTime;
+
+}
+
+const timeTracker = document.querySelector(".timetracker__time");
+
+setInterval(() => {
+  timeTracker.innerHTML = getCurrentTime();
+  }, 1000);
